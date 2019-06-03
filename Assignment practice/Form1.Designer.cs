4bc635edf06d0d7311eb506d02749cc19761d350
Robertrc0033048 <rc0033048@student.riversidecollege.ac.uk>
2019-06-03 15:15:21 +0100

    triangle added

--- Assignment practice/Form1.Designer.cs
+++ Assignment practice/Form1.Designer.cs
@@ -34,25 +34,49 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.AreaOut = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Areaout2 = new System.Windows.Forms.Label();
+            this.BaseBox = new System.Windows.Forms.TextBox();
+            this.Heightbox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.RadiusBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
             this.SuspendLayout();
             // 
             // WidthBox
             // 
-            this.WidthBox.Location = new System.Drawing.Point(151, 66);
+            this.WidthBox.Location = new System.Drawing.Point(134, 132);
             this.WidthBox.Name = "WidthBox";
             this.WidthBox.Size = new System.Drawing.Size(124, 20);
             this.WidthBox.TabIndex = 0;
             // 
             // HeightBox
             // 
-            this.HeightBox.Location = new System.Drawing.Point(151, 117);
+            this.HeightBox.Location = new System.Drawing.Point(134, 183);
             this.HeightBox.Name = "HeightBox";
             this.HeightBox.Size = new System.Drawing.Size(124, 20);
             this.HeightBox.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(322, 181);
+            this.button1.Location = new System.Drawing.Point(305, 247);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -63,7 +87,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 69);
+            this.label1.Location = new System.Drawing.Point(37, 135);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 3;
@@ -72,7 +96,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 120);
+            this.label2.Location = new System.Drawing.Point(37, 186);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 4;
@@ -81,27 +105,230 @@
             // AreaOut
             // 
             this.AreaOut.AutoSize = true;
-            this.AreaOut.Location = new System.Drawing.Point(148, 186);
+            this.AreaOut.Location = new System.Drawing.Point(131, 252);
             this.AreaOut.Name = "AreaOut";
             this.AreaOut.Size = new System.Drawing.Size(38, 13);
             this.AreaOut.TabIndex = 5;
             this.AreaOut.Text = "Area =";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(905, 570);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.WidthBox);
+            this.tabPage1.Controls.Add(this.AreaOut);
+            this.tabPage1.Controls.Add(this.HeightBox);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(727, 462);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Rectangle";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.BaseBox);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.Heightbox2);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.Areaout2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(727, 462);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Triangle";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.label9);
+            this.tabPage3.Controls.Add(this.label8);
+            this.tabPage3.Controls.Add(this.label7);
+            this.tabPage3.Controls.Add(this.RadiusBox);
+            this.tabPage3.Controls.Add(this.textBox2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(897, 544);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Circle";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Base";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Height";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Areaout2
+            // 
+            this.Areaout2.AutoSize = true;
+            this.Areaout2.Location = new System.Drawing.Point(132, 226);
+            this.Areaout2.Name = "Areaout2";
+            this.Areaout2.Size = new System.Drawing.Size(38, 13);
+            this.Areaout2.TabIndex = 9;
+            this.Areaout2.Text = "Area =";
+            // 
+            // BaseBox
+            // 
+            this.BaseBox.Location = new System.Drawing.Point(135, 123);
+            this.BaseBox.Name = "BaseBox";
+            this.BaseBox.Size = new System.Drawing.Size(100, 20);
+            this.BaseBox.TabIndex = 10;
+            // 
+            // Heightbox2
+            // 
+            this.Heightbox2.Location = new System.Drawing.Point(135, 169);
+            this.Heightbox2.Name = "Heightbox2";
+            this.Heightbox2.Size = new System.Drawing.Size(100, 20);
+            this.Heightbox2.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(135, 59);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "Base * Height / 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Equation";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Equation";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Width * Height";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "Pi r ^2";
+            // 
+            // RadiusBox
+            // 
+            this.RadiusBox.Location = new System.Drawing.Point(140, 111);
+            this.RadiusBox.Name = "RadiusBox";
+            this.RadiusBox.Size = new System.Drawing.Size(100, 20);
+            this.RadiusBox.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(68, 62);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Equation";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(68, 114);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Radius";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(140, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Area =";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(938, 578);
-            this.Controls.Add(this.AreaOut);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.HeightBox);
-            this.Controls.Add(this.WidthBox);
+            this.ClientSize = new System.Drawing.Size(905, 570);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -113,6 +340,26 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label AreaOut;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Areaout2;
+        private System.Windows.Forms.TextBox BaseBox;
+        private System.Windows.Forms.TextBox Heightbox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox RadiusBox;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Assignment practice/Form1.cs
+++ Assignment practice/Form1.cs
@@ -19,16 +19,65 @@ namespace Assignment_practice
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double Width, Height, Area;
+            //calculate the area of a rectangle
+            //tests the code for errors
+            try
+            {
+                double Width, Height, Area;
 
-            Width = Convert.ToDouble(WidthBox.Text);
+                Width = Convert.ToDouble(WidthBox.Text);
 
-            Height = Convert.ToDouble(HeightBox.Text);
+                Height = Convert.ToDouble(HeightBox.Text);
 
-            Area = Width * Height;
+                Area = Width * Height;
 
-            AreaOut.Text = "Area = " + Area;
+                AreaOut.Text = "Area = " + Area;
+            }
+            // will reply to the error that is found
+            catch
+            {
+                MessageBox.Show("type in two numbers!");
+            }
 
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Base, Height, Area;
+
+                Base = Convert.ToDouble(BaseBox.Text);
+
+                Height = Convert.ToDouble(Heightbox2.Text);
+
+                Area = Base * Height;
+
+                Area1 = Area / 2;
+
+                Areaout2.Text = "Area =" + Area1;
+            }
+            catch
+            {
+                MessageBox.Show("Type in Two Numbers!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Double Radius, Area;
+
+                Radius = Convert.ToDouble(RadiusBox.Text);
+
+                Area = 
+            }
         }
     }
 }
